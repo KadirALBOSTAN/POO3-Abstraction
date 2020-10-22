f7ee55fc195f98d9ef55f0dde9ee820a376618df
Kadir <kadir.albostan@gmail.com>
2020-10-22 14:51:58 +0200

    début quête

---
+++ MotorWay.php
@@ -0,0 +1,14 @@
+<?php
+
+final class MotorWay extends HighWay
+{
+    protected $nbLane = 4;
+
+    protected $maxSpeed = 130;
+
+    public function addVehicle(vehicle $vehicle)
+    {
+        
+    }
+
+}
